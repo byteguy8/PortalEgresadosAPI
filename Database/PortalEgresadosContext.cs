1af73ddf355dbdc9750a2d0553af82671497d3ef
Thomis Garrido <garridothomis@yahoo.com>
2023-11-12 12:19:11 -0400

    Conexion con la base de datos

--- Database/PortalEgresadosContext.cs
+++ Database/PortalEgresadosContext.cs
@@ -87,7 +87,7 @@ public partial class PortalEgresadosContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=PortalEgresados;Integrated Security=True;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=34.23.161.151;Initial Catalog=PortalEgresados;User ID=backend;Password=@uasd809@;Encrypt=false");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
