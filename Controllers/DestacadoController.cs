0f7378c37e81b574f9077c944bdd7e7407d337b7
Thomis Garrido <garridothomis@yahoo.com>
2023-11-21 21:25:10 -0400

    Adiccion del endpoint para buscar destacado por id

--- Controllers/DestacadoController.cs
+++ Controllers/DestacadoController.cs
@@ -7,6 +7,48 @@ namespace PortalEgresadosAPI.Controllers;
 [Route("[controller]")]
 public class DestacadoController : ControllerBase
 {
+
+    [HttpGet("{IdEgresado}")]
+    public IResult ObtenerDestacadoPorId(int IdEgresado)
+    {
+        PortalEgresadosContext? context = null;
+
+        try
+        {
+            context = new PortalEgresadosContext();
+
+            var rawDestacados = context
+                .EgresadoDestacados
+                .Include(d => d.Egresado.Participante)
+                .Include(d => d.Egresado.NacionalidadNavigation)
+                .Where(d => d.FechaHasta >= DateTime.Now && d.EgresadoId == IdEgresado)
+                .OrderBy(d => d.Egresado.PrimerNombre)
+                .ToList()
+            ?? throw Utils.APIError(
+                "Hubo un error al procesar la solicitud. Intentelo de nuevo",
+                StatusCodes.Status500InternalServerError
+            );
+
+            var egresados = new List<dynamic>();
+
+            foreach (var rawDestacado in rawDestacados)
+            {
+                var rawEgresado = rawDestacado.Egresado;
+                egresados.Add(Utils.ObtenerInfEgresado(context, rawEgresado, true));
+            }
+
+            return Results.Ok(egresados);
+        }
+        catch (Exception ex)
+        {
+            return Utils.HandleError(ex);
+        }
+        finally
+        {
+            context?.Dispose();
+        }
+    }
+
     [HttpGet]
     public IResult ObtenerDestacados()
     {
